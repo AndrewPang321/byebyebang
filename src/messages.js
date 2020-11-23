c5313a7b423ae9e0da985cc6b52655c58742aa06
sinwailam193 <aaronsinwailam@gmail.com>
2020-11-23 20:09:46 +0800

    see you again bang

--- src/messages.js
+++ src/messages.js
@@ -17,4 +17,12 @@ export const messages = [
     coverImg: "./andrewcover.jpg",
     profileImg: "./andrewprofile.jpg",
   },
+  {
+    title: "See you again Bang",
+    msg: "I remember when you first interviewed my for Carousell man, thanks for saying yes to my application. From that point on it sparked an adventure and I got to know Carousell and you and the rest of the web team. Thanks for not doubting me and thank you for all the help that you have given me throuhg the years, wish you nothing but the best.",
+    rel: "Imposter",
+    name: "Aaron",
+    coverImg: "./aaronlamcover.jpg",
+    profileImg: "./aaronlam.jpeg",
+  }
 ];
